c6640f9ba4d620abebee60906a95c09ffe3417cf
kalki39 <kalkiram39@gmail.com>
2022-12-30 23:16:09 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function submitform(){
+let flname=document.getElementById('fname').value;
+let ltname=document.getElementById('lname').value;
+let Email=document.getElementById('email').value;
+let Reason=document.getElementById('reason').value;
+
+if(flname!='' && ltname!='' && Email!='' && Reason!=''){
+    alert('FORM SUBMITED SUCCESSFULLY')
+}
+else{
+    
+}
+}
+
+
